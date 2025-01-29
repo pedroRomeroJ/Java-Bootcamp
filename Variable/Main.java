b92e1d80b2546356a870669c000536db2f4c9a3a
Pedro Romero <pjr616039@gmail.com>
2025-01-29 13:43:13 -0600

    commit for variables file

---
+++ Variable/Main.java
@@ -0,0 +1,23 @@
+package Variable;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Primitive vs Reference
+
+        int age = 30;
+
+        System.out.println("My age is " + age);
+
+        char grade = 'A';
+
+
+        boolean isStudent = false;
+
+        System.out.println("Am I a student = " +isStudent);
+
+        String name = "Pedro";
+
+        System.out.println("My name is "+ name);
+    }
+}
