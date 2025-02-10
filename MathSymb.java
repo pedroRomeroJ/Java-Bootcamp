b470cb2136379bafa62cdf368747674d8ee9b58c
Pedro Romero <pjr616039@gmail.com>
2025-02-10 17:08:24 -0600

    Math Symbols

---
+++ MathSymb.java
@@ -0,0 +1,23 @@
+public class MathSymb {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result1;
+        double result2;
+        double result3;
+        double result4;
+
+        result1 = Math.pow(2,5);
+        result2 = Math.abs(-5);
+        result3 = Math.sqrt(9);
+        result4 = Math.round(3.14); // round up Math.ceil(), round down Math.floor()
+        // Math.max(), Math.min()
+
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
