bb88569ab7132ea080e6295115137c1adb4b4310
Pedro Romero <pjr616039@gmail.com>
2025-02-10 15:15:29 -0600

    Shopping Cart program

---
+++ Shoppping.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Shoppping {
+    public static void main(String[] args) {
+        //SHOPPING CART PROGRAM
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("You have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+
+
+
+        //System.out.println(total);
+
+
+
+
+
+
+        scanner.close();
+    }
+}
